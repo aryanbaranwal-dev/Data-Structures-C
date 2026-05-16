e2852622e2bd35cf4ea9a5c2464edfc0b7f4e2cf
Aryan Baranwal <aryanbarnwal98@gmail.com>
2026-05-16 14:57:43 +0530

    Solve LeetCode 53: Maximum Subarray

---
+++ 53_max_subarray.c
@@ -0,0 +1,19 @@
+#include <math.h>
+int maxSubArray(int* nums, int numsSize) {
+    int max_sum = nums[0];
+    int current_sum = nums[0];
+    
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[i] > current_sum + nums[i]) {
+            current_sum = nums[i];
+        } else {
+            current_sum = current_sum + nums[i];
+        }
+        
+        if (current_sum > max_sum) {
+            max_sum = current_sum;
+        }
+    }
+    
+    return max_sum;
+}
