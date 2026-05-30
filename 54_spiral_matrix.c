4b46c376b5721f0fab91f3d203bce5aa50d25633
Aryan Baranwal <aryanbarnwal98@gmail.com>
2026-05-30 22:59:30 +0530

    Solve LeetCode 54: Spiral Matrix

---
+++ 54_spiral_matrix.c
@@ -0,0 +1,48 @@
+int* spiralOrder(int** matrix, int matrixSize, int* matrixColSize, int* returnSize) {
+    if (matrixSize == 0 || matrixColSize[0] == 0) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    int rows = matrixSize;
+    int cols = matrixColSize[0];
+    int totalElements = rows * cols;
+    
+    int* result = (int*)malloc(totalElements * sizeof(int));
+    *returnSize = totalElements;
+
+    int top = 0;
+    int bottom = rows - 1;
+    int left = 0;
+    int right = cols - 1;
+    int index = 0;
+
+    while (top <= bottom && left <= right) {
+    
+        for (int i = left; i <= right; i++) {
+            result[index++] = matrix[top][i];
+        }
+        top++;
+
+        for (int i = top; i <= bottom; i++) {
+            result[index++] = matrix[i][right];
+        }
+        right--;
+
+        if (top <= bottom) {
+            for (int i = right; i >= left; i--) {
+                result[index++] = matrix[bottom][i];
+            }
+            bottom--;
+        }
+
+        if (left <= right) {
+            for (int i = bottom; i >= top; i--) {
+                result[index++] = matrix[i][left];
+            }
+            left++;
+        }
+    }
+
+    return result;
+}
